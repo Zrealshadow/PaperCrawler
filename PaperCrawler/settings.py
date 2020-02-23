c52d412c5c7854098075b6fb7e38325c35c36894
Zreal <zenglz_pro@163.com>
2020-02-23 18:22:46 +0800

    设置log显示在文件中

--- PaperCrawler/settings.py
+++ PaperCrawler/settings.py
@@ -88,3 +88,12 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+from datetime import datetime
+#用时间戳进行标注
+today=datetime.now().timestamp()
+log_file_path="./log/scrapy_{}_.log".format(str(today.year))
+
+#log output
+LOG_LEVEL='DEBUG'
+LOG_FILE=log_file_path
